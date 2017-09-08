81e13a1647ab7f26bc0ab07e3b1988c67ebc6552
Qin Wang (Beyondsoft) <v-wanqin@microsoft.com>
2017-09-08 17:19:30 +0800

    language change

---
+++ TestC-E/TestC-E/Controllers/TestController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestC_E.Helpers;
+using TestC_E.Models;
+
+namespace TestC_E.Controllers
+{
+    public class TestController : BaseController
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Person per)
+        {
+            return View();
+        }
+        //public ActionResult SetCulture(string culture)
+        //{
+        //    // Validate input
+        //    culture = CultureHelper.GetImplementedCulture(culture);
+        //    // Save culture in a cookie
+        //    HttpCookie cookie = Request.Cookies["_culture"];
+        //    if (cookie != null)
+        //        cookie.Value = culture;   // update cookie value
+        //    else
+        //    {
+        //        cookie = new HttpCookie("_culture");
+        //        cookie.Value = culture;
+        //        cookie.Expires = DateTime.Now.AddYears(1);
+        //    }
+        //    Response.Cookies.Add(cookie);
+        //    return RedirectToAction("Index");
+        //}
+
+        public ActionResult SetCulture(string culture)
+        {
+            // Validate input
+            culture = CultureHelper.GetImplementedCulture(culture);
+            RouteData.Values["culture"] = culture;  // set culture
+
+
+            return RedirectToAction("Index");
+        }
+    }
+}
